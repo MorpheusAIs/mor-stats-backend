3bf1015c9c44ae1138b087acbd64d6afc452d36f
bowtiedswan <bowtiedswan@gmail.com>
2025-01-30 10:44:03 +0200

    Enhance Gunicorn configuration with detailed settings and comments

--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/site/wwwroot
-source antenv/bin/activate
-gunicorn --bind=0.0.0.0:8000 --timeout 600 --access-logfile '-' --error-logfile '-' --workers 4 -k uvicorn.workers.UvicornWorker main:app 
